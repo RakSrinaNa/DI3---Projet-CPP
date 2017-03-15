717d24277702cca2dac7c7c1fd5223c018e764ff
NoahReyson <shadow.reyson@gmail.com>
2017-03-15 18:07:16 +0100

    trucs

--- Projet Matrice/Projet Matrice/Projet Matrice.cpp
+++ Projet Matrice/Projet Matrice/Projet Matrice.cpp
@@ -1,5 +1,11 @@
+#include "CMatrix.h"
+#include "CMatrixParser.h"
+
 int main(int argc, char* argv[])
 {
+	char * file = "M1.txt";
+	CMatrix<double> matrix = CMatrixParser::PMTXreadFile<double>(file);
+	matrix.MTXdisplay();
 	return 0;
 }
 
