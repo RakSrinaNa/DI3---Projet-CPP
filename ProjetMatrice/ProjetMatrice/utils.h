7c6024696d4682e3c78cff5976b822ed20452bb7
Thomas Couchoud <zerderr@gmail.com>
2017-03-26 16:01:32 +0200

    Utils comments

--- ProjetMatrice/ProjetMatrice/utils.h
+++ ProjetMatrice/ProjetMatrice/utils.h
@@ -3,15 +3,17 @@
 
 #define UNUSED(x) (void)(x)
 
+// Malloc utils, to get the memory space and verify it.
 #define MALLOC(t, n) (t *) malloc((n) * sizeof(t))
 #define MCHECK(x, m) if(!x){perror("MALLOC ERROR"); if(m)perror(m); exit(EXIT_FAILURE);}
+#define MMALLOC(p, t, n, m) p = MALLOC(t, n); MCHECK(p, m);
 
+// Realloc utils, to get the memory space and verify it.
 #define REALLOC(t, p, n) (t *) realloc(p, (n) * sizeof(t))
 #define RCHECK(x, m) if(!x){perror("REALLOC ERROR"); if(m)perror(m); exit(EXIT_FAILURE);}
-
-#define MMALLOC(p, t, n, m) p = MALLOC(t, n); MCHECK(p, m);
 #define RREALLOC(p, t, n, m) p = REALLOC(t, p, n); RCHECK(p, m);
 
+// Define functions for Visual studio or UNIX (else)
 #ifdef _MSC_VER
 	#define STRDUP(t) _strdup(t)
 	#define STRCMPI(b, t) _strcmpi(b, t)
