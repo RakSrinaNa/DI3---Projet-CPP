ba8aff542e0c014bad291ee55fc4c66ad3d6a414
Thomas Couchoud <zerderr@gmail.com>
2017-04-07 17:17:59 +0200

    Add parser unit

---
+++ ProjetGraph/ProjetGraph/tests/CGraphParserUnit.cpp
@@ -0,0 +1,8 @@
+#include "CGraphParserUnit.h"
+#include "../CGraphParser.h"
+
+void CGraphParserUnit::PGRAUnitTests()
+{
+	CGraph * graph = CGraphParser::PGRAreadGraph((char *) "g1.txt");
+	int a;
+}
---
+++ ProjetGraph/ProjetGraph/tests/CGraphParserUnit.h
@@ -0,0 +1,10 @@
+#ifndef PROJETGRAPH_CGRAPHPARSERUNIT_H
+#define PROJETGRAPH_CGRAPHPARSERUNIT_H
+
+class CGraphParserUnit
+{
+public:
+	static void PGRAUnitTests();
+};
+
+#endif
