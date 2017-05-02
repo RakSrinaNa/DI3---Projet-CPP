fe693071cbccb386f26109fb39806849837b1b5e
NoahReyson <shadow.reyson@gmail.com>
2017-05-02 17:23:18 +0200

    push

--- ProjetGraph/ProjetGraph/CArc.cpp
+++ ProjetGraph/ProjetGraph/CArc.cpp
@@ -6,6 +6,10 @@ CArc::CArc()
 	throw CException(MISSING_ARC_INDEX_EXCEPTION, (char *) "Use the constructor : CArc(unsigned int)");
 }
 
+CArc::CArc(CArc const& oARCarcParam) : uiVertexIndex(oARCarcParam.uiVertexIndex)
+{
+}
+
 CArc::CArc(unsigned int uiVertexIndexParam) : uiVertexIndex(uiVertexIndexParam)
 {
 }
