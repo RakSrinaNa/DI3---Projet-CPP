d299261ce2f157126015799d96a13a9df4aa4175
Thomas Couchoud <zerderr@gmail.com>
2017-05-10 17:17:06 +0200

    Prepare toolbox

---
+++ ProjetGraph/ProjetGraph/CGraphToolbox.cpp
@@ -0,0 +1,90 @@
+#include "CGraphToolbox.h"
+#include "CException.h"
+
+/**************************************************************
+ * Default constructor. Do not use. Prefer CGraphToolbox(CGraph *).
+ **************************************************************
+ *
+ * Input:
+ * Output:
+ * PreCond:
+ * PostCond:
+ *      Throws a CException with the ID `NO_GRAPH_EXCEPTION`.
+ */
+CGraphToolbox::CGraphToolbox()
+{
+	throw CException(NO_GRAPH_EXCEPTION, (char *) "No graph were given.");
+}
+
+/**************************************************************
+ * Confort constructor taking a graph.
+ **************************************************************
+ *
+ * Input:
+ *      poGRAgraph: The graph we'll use. A copy will be made.
+ * Output:
+ * PreCond:
+ * PostCond:
+ */
+CGraphToolbox::CGraphToolbox(CGraph const &graph)
+{
+	oGRAgraph = graph;
+}
+
+/**************************************************************
+ * Destructor.
+ **************************************************************
+ *
+ * Input:
+ * Output:
+ * PreCond:
+ * PostCond:
+ */
+CGraphToolbox::~CGraphToolbox()
+{
+}
+
+/**************************************************************
+ * Get the current graph.
+ **************************************************************
+ *
+ * Input:
+ * Output:
+ *      CGraph: The current graph.
+ * PreCond:
+ * PostCond:
+ */
+CGraph const &CGraphToolbox::GRTgetGraph() const
+{
+	return oGRAgraph;
+}
+
+/**************************************************************
+ * Transform the current graph into a non oriented one.
+ **************************************************************
+ *
+ * Input:
+ * Output:
+ * PreCond:
+ * PostCond:
+ */
+void CGraphToolbox::GRTtransformNonOriented()
+{
+	//TODO
+}
+
+/**************************************************************
+ * Tell if the graph is connected.
+ **************************************************************
+ *
+ * Input:
+ * Output:
+ *      bool: True if the graph is connected, false else.
+ * PreCond:
+ * PostCond:
+ */
+bool CGraphToolbox::isConnex()
+{
+	GRTtransformNonOriented();
+	return false; //TODO
+}
---
+++ ProjetGraph/ProjetGraph/CGraphToolbox.h
@@ -0,0 +1,85 @@
+#ifndef PROJETGRAPH_CGRAPHTOOLBOX_H
+#define PROJETGRAPH_CGRAPHTOOLBOX_H
+
+#include "CGraph.h"
+
+#define NO_GRAPH_EXCEPTION 9876
+
+class CGraphToolbox
+{
+private:
+	CGraph oGRAgraph;
+
+public:
+	/**************************************************************
+	 * Default constructor. Do not use. Prefer CGraphToolbox(CGraph *).
+	 **************************************************************
+	 *
+	 * Input:
+	 * Output:
+	 * PreCond:
+	 * PostCond:
+	 *      Throws a CException with the ID `NO_GRAPH_EXCEPTION`.
+	 */
+	CGraphToolbox();
+	
+	/**************************************************************
+	 * Confort constructor taking a graph.
+	 **************************************************************
+	 *
+	 * Input:
+	 *      poGRAgraph: The graph we'll use. A copy will be made.
+	 * Output:
+	 * PreCond:
+	 * PostCond:
+	 */
+	explicit CGraphToolbox(CGraph const &graph);
+	
+	/**************************************************************
+	 * Destructor.
+	 **************************************************************
+	 *
+	 * Input:
+	 * Output:
+	 * PreCond:
+	 * PostCond:
+	 */
+	~CGraphToolbox();
+	
+	/**************************************************************
+	 * Get the current graph.
+	 **************************************************************
+	 *
+	 * Input:
+	 * Output:
+	 *      CGraph: The current graph.
+	 * PreCond:
+	 * PostCond:
+	 */
+	CGraph const& GRTgetGraph() const;
+	
+	/**************************************************************
+	 * Transform the current graph into a non oriented one.
+	 **************************************************************
+	 *
+	 * Input:
+	 * Output:
+	 * PreCond:
+	 * PostCond:
+	 */
+	void GRTtransformNonOriented();
+	
+	/**************************************************************
+	 * Tell if the graph is connected.
+	 **************************************************************
+	 *
+	 * Input:
+	 * Output:
+	 *      bool: True if the graph is connected, false else.
+	 * PreCond:
+	 * PostCond:
+	 */
+	bool isConnex();
+};
+
+#endif
