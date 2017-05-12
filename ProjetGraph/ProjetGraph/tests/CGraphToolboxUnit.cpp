0e6fe8a8ea471e9ba05fc458606c89e9f05d547c
Thomas Couchoud <zerderr@gmail.com>
2017-05-12 08:57:36 +0200

    Add a test, @NoahReyson va falloir fix

--- ProjetGraph/ProjetGraph/tests/CGraphToolboxUnit.cpp
+++ ProjetGraph/ProjetGraph/tests/CGraphToolboxUnit.cpp
@@ -1,6 +1,14 @@
 #include "CGraphToolboxUnit.h"
+#include "../CGraph.h"
+#include "../CGraphToolbox.h"
+#include "CUnit.h"
 
 void CGraphToolboxUnit::GRTUnitTests()
 {
-
+	CGraph oGRAgraph1 = CGraph((char *) "g3.txt");
+	CGraphToolbox oGRTtoolbox = CGraphToolbox(oGRAgraph1);
+	
+	oGRTtoolbox.GRTtransformNonOriented();
+	if(!oGRTtoolbox.GRTgetGraph().GRAhasArc(1, 2) || !oGRTtoolbox.GRTgetGraph().GRAhasArc(2, 1) || !oGRTtoolbox.GRTgetGraph().GRAhasArc(2, 3) || !oGRTtoolbox.GRTgetGraph().GRAhasArc(3, 2) || !oGRTtoolbox.GRTgetGraph().GRAhasArc(3, 4) || !oGRTtoolbox.GRTgetGraph().GRAhasArc(4, 3) || !oGRTtoolbox.GRTgetGraph().GRAhasArc(4, 5) || !oGRTtoolbox.GRTgetGraph().GRAhasArc(5, 4) || !oGRTtoolbox.GRTgetGraph().GRAhasArc(5, 1) || !oGRTtoolbox.GRTgetGraph().GRAhasArc(1, 5))
+		CUnit::UNITassertError("ASSERT CGRAPHTOOLBOX 1");
 }
