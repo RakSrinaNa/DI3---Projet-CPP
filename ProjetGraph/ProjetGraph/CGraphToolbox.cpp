d3b10d8f54ead7f3a33b574ee2d5caa33d593c6c
NoahReyson <shadow.reyson@gmail.com>
2017-05-20 18:13:54 +0200

    test 56

--- ProjetGraph/ProjetGraph/CGraphToolbox.cpp
+++ ProjetGraph/ProjetGraph/CGraphToolbox.cpp
@@ -172,6 +172,11 @@ bool CGraphToolbox::GRThasPath(unsigned int uiStartIndex, unsigned int uiEndInde
 	unsigned int * puiReachableIndices;
 	unsigned int uiReachableSize = oGRAgraph.GRAgetReachableIndices(uiStartIndex, &puiReachableIndices);
 
+	std::printf("Indice %d : ", uiStartIndex);
+	for(unsigned int i = 0; i < uiReachableSize; i++)
+		std::printf("%d ", puiReachableIndices[i]);
+	std::printf("\n");
+	
 	(*puiAlreadyExplored)[0]++;
 	RREALLOC(*puiAlreadyExplored, unsigned int, (*puiAlreadyExplored)[0] + 1, "GRThasPath");
 	(*puiAlreadyExplored)[(*puiAlreadyExplored)[0]] = uiStartIndex;
